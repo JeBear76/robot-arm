f87021f7716cbcdf3c4f6869ec50d1ffd993d2a8
JeBear76 <jerepondumie@gmail.com>
2019-08-26 22:51:13 +0200

    Changed setting increments by sending digit 0-9 Changed constant names Refined setSleep to prevent hooking the claw servo on the base

--- robot-arm.ino
+++ robot-arm.ino
@@ -4,40 +4,41 @@
 #define CLAW 11
 #define CLAW_INIT 329
 #define CLAW_SLEEP 330
-#define CLAW_CLOSED 465
-#define CLAW_OPEN 290
+#define CLAW_MAX 465    // Closed
+#define CLAW_MIN 290    // Open
 
 #define WRIST 10
 #define WRIST_INIT 339
 #define WRIST_SLEEP 340
-#define WRIST_LEFT 580
-#define WRIST_RIGHT 100
+#define WRIST_MAX 580
+#define WRIST_MIN 100
  
 #define WRIST_TILT 9
 #define WRIST_TILT_INIT 579
-#define WRIST_TILT_SLEEP 400
-#define WRIST_UP 580
-#define WRIST_DOWN 100
+#define WRIST_TILT_SLEEP 250
+#define WRIST_TILT_MAX 580
+#define WRIST_TILT_MIN 100
 
 #define ELBOW 7
 #define ELBOW_INIT 449
 #define ELBOW_SLEEP 450
-#define ELBOW_STRAIGHT 200
-#define ELBOW_BENT 450
+#define ELBOW_MAX 450  // Bent
+#define ELBOW_MIN 200  // Straight
 
 #define SHOULDER_TILT 6
 #define SHOULDER_TILT_INIT 299
 #define SHOULDER_TILT_SLEEP 500
-#define SHOULDER_UP 350
-#define SHOULDER_DOWN 160
+#define SHOULDER_TILT_MAX 350
+#define SHOULDER_TILT_MIN 160
 
 #define SHOULDER 5
 #define SHOULDER_INIT 359
 #define SHOULDER_SLEEP 120
-#define SHOULDER_LEFT 600
-#define SHOULDER_RIGHT 120
+#define SHOULDER_MAX 600
+#define SHOULDER_MIN 120
 
-int testservo = 5;
+bool inSleepPosition = true;
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 int currentClaw = CLAW_INIT;
@@ -46,18 +47,19 @@ int currentWristTilt = WRIST_TILT_INIT;
 int currentElbow = ELBOW_INIT;
 int currentShoulderTilt = SHOULDER_TILT_INIT;
 int currentShoulder = SHOULDER_INIT;
-byte increment = 10;
+byte increment = 10;    // Default increment to avoid breaking things on first use
 
 void setup() {
   Serial.begin(9600);
-  // put your setup code here, to run once:
   pwm.begin();
   pwm.setPWMFreq(60);
   delay(10);
-
   setAwake();
 }
 
+/*
+ * Activates teh appropriate servo
+ */
 int changePulse(byte servo, int startPulse, int endPulse){
   if(startPulse < endPulse){
     for (uint16_t pulselen = startPulse; pulselen < endPulse; pulselen++) {
@@ -75,16 +77,30 @@ int changePulse(byte servo, int startPulse, int endPulse){
   return endPulse;
 }
 
+/*
+ * Moves teh arm to its sleep position
+ */
 void setSleep(){
+  currentShoulderTilt = changePulse(SHOULDER_TILT, currentShoulderTilt, SHOULDER_TILT_MAX);
   currentClaw = changePulse(CLAW, currentClaw, CLAW_SLEEP);
   currentWrist = changePulse(WRIST, currentWrist, WRIST_SLEEP);
-  currentWristTilt = changePulse(WRIST_TILT, currentWristTilt, WRIST_TILT_SLEEP);
+  currentWristTilt = changePulse(WRIST_TILT, currentWristTilt, WRIST_TILT_INIT);
   currentElbow = changePulse(ELBOW, currentElbow, ELBOW_SLEEP);
+  currentShoulder = changePulse(SHOULDER, currentShoulder, SHOULDER_SLEEP);
   currentShoulderTilt = changePulse(SHOULDER_TILT, currentShoulderTilt, SHOULDER_TILT_SLEEP);
-  currentShoulder = changePulse(SHOULDER, currentShoulder, SHOULDER_SLEEP);    
+  currentWristTilt = changePulse(WRIST_TILT, currentWristTilt, WRIST_TILT_SLEEP);
 }
 
+/*
+ * Moves teh arm to its start position
+ */
 void setAwake(){
+  // Will do nothing if the arm isn't in sleep position
+  // This is to prevent it jerking around and damaging itself
+  if(!inSleepPosition){
+    return;
+  }
+  inSleepPosition = false;
   currentClaw = changePulse(CLAW, CLAW_SLEEP, CLAW_INIT);
   currentWrist = changePulse(WRIST, WRIST_SLEEP, WRIST_INIT);
   currentWristTilt = changePulse(WRIST_TILT, WRIST_TILT_SLEEP, WRIST_TILT_INIT);
@@ -93,6 +109,9 @@ void setAwake(){
   currentShoulder = changePulse(SHOULDER, SHOULDER_SLEEP, SHOULDER_INIT);  
 }
 
+/*
+ * Sets the endPulse for the movement based on action
+ */
 int setReqActionEndPulse(int endPulse, byte action){
   if(action >= 97 && action <= 122){
     endPulse -= increment;
@@ -103,7 +122,10 @@ int setReqActionEndPulse(int endPulse, byte action){
   return endPulse;
 }
 
-int setReqEndPulse(int endPulse, int minPulse, int maxPulse){
+/*
+ * Keeps the endPulse for the movement within the boundaries of the servo
+ */
+int validateReqEndPulse(int endPulse, int minPulse, int maxPulse){
   if(endPulse > maxPulse){
     endPulse = maxPulse;
   }
@@ -117,17 +139,18 @@ void loop() {
   byte tmp = 255;
   byte reqAction = 0;
   int reqEndPulse = 0;
+  
   if(Serial.available()){
     tmp = Serial.read();
     if(tmp == 10){
       return;
     }
-    if(tmp == 37)
+    if(tmp == 37) // %
     {
-      setSleep();
+      setSleep(); 
       return;
     }
-    if(tmp == 38)
+    if(tmp == 38) // &
     {
       setAwake();
       return;
@@ -135,79 +158,67 @@ void loop() {
     Serial.print("setting servo action: ");
     reqAction = tmp;    
     Serial.println(reqAction);
-  }
-  switch(reqAction){
-    case 105:{
-      increment -= 5;
-      if(increment < 5){
-        increment = 5;
-      }
+    if(reqAction >= 48 && reqAction <= 57)  // Digits 0 to 9
+    {
+      increment = (reqAction - '0' + 1) * 5;
       Serial.println(increment);
-      break;
     }
-    case 73:{
-      increment += 5;
-      if (increment > 50){
-        increment = 50;
-      }
-      Serial.println(increment);
-      break;
-    }    
-    case 67:
-    case 99:{
-      Serial.println("CLAW");
-      reqEndPulse = setReqActionEndPulse(currentClaw, reqAction);
-      reqEndPulse = setReqEndPulse(reqEndPulse, CLAW_OPEN, CLAW_CLOSED);
-      currentClaw = changePulse(CLAW, currentClaw, reqEndPulse);
-      break;
-    } 
-    case 87:
-    case 119:
-    {
-      Serial.println("WRIST");
-      reqEndPulse = setReqActionEndPulse(currentWrist, reqAction);
-      reqEndPulse = setReqEndPulse(reqEndPulse, WRIST_RIGHT, WRIST_LEFT);
-      currentWrist = changePulse(WRIST, currentWrist, reqEndPulse);
-      break;
-    } 
-    case 88:
-    case 120:
-    {
-      Serial.println("WRIST_TILT");
-      reqEndPulse = setReqActionEndPulse(currentWristTilt, reqAction);
-      reqEndPulse = setReqEndPulse(reqEndPulse, WRIST_DOWN, WRIST_UP);
-      currentWristTilt = changePulse(WRIST_TILT, currentWristTilt, reqEndPulse);
-      break;
-    } 
-    case 69:
-    case 101:{
-      Serial.println("ELBOW");
-      reqEndPulse = setReqActionEndPulse(currentElbow, reqAction);
-      reqEndPulse = setReqEndPulse(reqEndPulse, ELBOW_STRAIGHT, ELBOW_BENT);
-      currentElbow = changePulse(ELBOW, currentElbow, reqEndPulse);
-      break;
-    } 
-    case 84:
-    case 116:
-    {
-      Serial.println("SHOULDER_TILT");
-      reqEndPulse = setReqActionEndPulse(currentShoulderTilt, reqAction);
-      reqEndPulse = setReqEndPulse(reqEndPulse, SHOULDER_DOWN, SHOULDER_UP);
-      currentShoulderTilt = changePulse(SHOULDER_TILT, currentShoulderTilt, reqEndPulse);
-      break;
-    } 
-    case 83:
-    case 115:
-    {
-      Serial.println("SHOULDER");
-      reqEndPulse = setReqActionEndPulse(currentShoulder, reqAction);
-      reqEndPulse = setReqEndPulse(reqEndPulse, SHOULDER_RIGHT, SHOULDER_LEFT);
-      currentShoulder = changePulse(SHOULDER, currentShoulder, reqEndPulse);
-      break;
-    } 
+    // One letter per servo. Uppercase for increment and lowercase for decrement
+    switch(reqAction){
+      case 67:  // C
+      case 99:  // c
+      {
+//        Serial.println("CLAW");
+        reqEndPulse = setReqActionEndPulse(currentClaw, reqAction);
+        reqEndPulse = validateReqEndPulse(reqEndPulse, CLAW_MIN, CLAW_MAX);
+        currentClaw = changePulse(CLAW, currentClaw, reqEndPulse);
+        break;
+      } 
+      case 87:  // W
+      case 119: // w
+      {
+//        Serial.println("WRIST");
+        reqEndPulse = setReqActionEndPulse(currentWrist, reqAction);
+        reqEndPulse = validateReqEndPulse(reqEndPulse, WRIST_MIN, WRIST_MAX);
+        currentWrist = changePulse(WRIST, currentWrist, reqEndPulse);
+        break;
+      } 
+      case 88:  // X
+      case 120: // x
+      {
+//        Serial.println("WRIST_TILT");
+        reqEndPulse = setReqActionEndPulse(currentWristTilt, reqAction);
+        reqEndPulse = validateReqEndPulse(reqEndPulse, WRIST_TILT_MIN, WRIST_TILT_MAX);
+        currentWristTilt = changePulse(WRIST_TILT, currentWristTilt, reqEndPulse);
+        break;
+      } 
+      case 69:  //E
+      case 101: //e
+      {
+//        Serial.println("ELBOW");
+        reqEndPulse = setReqActionEndPulse(currentElbow, reqAction);
+        reqEndPulse = validateReqEndPulse(reqEndPulse, ELBOW_MIN, ELBOW_MAX);
+        currentElbow = changePulse(ELBOW, currentElbow, reqEndPulse);
+        break;
+      } 
+      case 84:  // T
+      case 116: // t
+      {
+//        Serial.println("SHOULDER_TILT");
+        reqEndPulse = setReqActionEndPulse(currentShoulderTilt, reqAction);
+        reqEndPulse = validateReqEndPulse(reqEndPulse, SHOULDER_TILT_MIN, SHOULDER_TILT_MAX);
+        currentShoulderTilt = changePulse(SHOULDER_TILT, currentShoulderTilt, reqEndPulse);
+        break;
+      } 
+      case 83:  // S
+      case 115: // s
+      {
+//        Serial.println("SHOULDER");
+        reqEndPulse = setReqActionEndPulse(currentShoulder, reqAction);
+        reqEndPulse = validateReqEndPulse(reqEndPulse, SHOULDER_MIN, SHOULDER_MAX);
+        currentShoulder = changePulse(SHOULDER, currentShoulder, reqEndPulse);
+        break;
+      } 
+    }  
   }
-//  delay(10000);
-//  setSleep();
-//  delay(10000);
-//  setAwake();
 }
